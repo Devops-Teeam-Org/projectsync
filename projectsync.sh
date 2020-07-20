d8bd789b42aa78a721bb1d6cad0ac941b6f4f08e
najitestech <42984888+najitestech@users.noreply.github.com>
2020-07-20 10:30:56 -0400

    Create projectsync.sh

---
+++ projectsync.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+PID="/usr/sbin/projectsync/pid"
+
+echo 1 > ${PID}
+
+if [ -e $PID ]; then
+        line=$(cat ${PID})
+                while [ ${line} -gt 0 ]; do
+                        /usr/bin/inotifywait -e modify,create,delete -r /var/lib/awx/projects && \
+                        for i in `cat /usr/sbin/projectsync/host`; do /usr/bin/rsync -raluSHog --delete-after --ignore-errors --force /var/lib/awx/projects/ $i:/var/lib/awx/projects/; done
+
+                        line=$(cat ${PID})
+
+                done
+
+else
+        echo "PID file does not exist"
+fi
