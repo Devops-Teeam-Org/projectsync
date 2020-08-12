e365b681c5dc4e358235c4e26239353d0ff8d3ab
najitestech <42984888+najitestech@users.noreply.github.com>
2020-08-12 10:24:53 -0400

    Update projectsync.sh

--- projectsync.sh
+++ projectsync.sh
@@ -17,3 +17,25 @@ if [ -e $PID ]; then
 else
         echo "PID file does not exist"
 fi
+
+##DATABASE REPLICATION
+
+#!/usr/bin/bash
+
+PID="/usr/sbin/projectsync/pid"
+
+echo 1 > ${PID}
+
+if [ -e $PID ]; then
+        line=$(cat ${PID})
+                while [ ${line} -gt 0 ]; do
+                        /usr/bin/inotifywait -e modify,create,delete -r /var/lib/pgsql/data && \
+                        for i in `cat /usr/sbin/projectsync/host`; do /usr/bin/rsync -raluSHog --delete-after --ignore-errors --force /var/lib/pgsql/data/ $i:/var/lib/pgsql/data/; done
+
+                        line=$(cat ${PID})
+
+                done
+
+else
+        echo "PID file does not exist"
+fi
